85dc3a882bfd4877421c84106da04a116a8a32aa
Santosh <gudla.santosh3@gmail.com>
2020-09-06 01:04:30 +0530

    Part 53 Category tags pages and pagination implementations

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -7,20 +7,7 @@
 				<div class="row">
 					<div class="col-12 col-md-10 col-lg-8">
 						<div class="row">
-                            @if(count($categories) > 0)
-                                @foreach($categories as $nav)
-                                    <div class="col-12 col-md-4 col-lg-4">
-                                        <a href="">
-                                            <div class="banner_box">
-                                                <i class="fab fa-sketch"></i>
-                                                <h3 class="banner_box_h3">{{$nav->categoryName}}</h3>
-                                                <!-- <p>The Toptal Blog is the top hub for developers.</p> -->
-                                            </div>
-                                        </a>
-							        </div>
-
-                                @endforeach
-                            @endif
+                            <h1 class="white">Explore all blogs from our awesome website</h1>
 						</div>
 					</div>
 				</div>
@@ -83,10 +70,7 @@
                         @endif
 						
                     </div>
-                    <div class="text-center">
-                        <button><a href="/blogs">View All</button>
-
-                    </div>
+                    {!! $blogs->links() !!}
 				</div>
 			</div>
     
